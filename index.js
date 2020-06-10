17de9f65187275294f66d92829df41e00a648ae2
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-10 14:12:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const testVar = {}
-
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
 function testFunc() {
   return "hi"
+}
+function superbowlWin(record){
+  let winner= record.find(won => won.result =='W')
+  if(!!winner)
+   {
+     return winner.year;}
+    else{
+    return undefined;}
+  
 }
