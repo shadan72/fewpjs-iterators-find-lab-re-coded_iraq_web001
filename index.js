831115db09c680c5eed7af5e5dbaa33787820c0b
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-10 14:04:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const testVar = {}
-
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
 function testFunc() {
   return "hi"
 }
-function superbowlWin(array){
-  return array.find(won => array.)
+function superbowlWin(record){
+  return record.find(won => won.result ==='W')
   )
 }
